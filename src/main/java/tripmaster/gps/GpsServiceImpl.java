3d0e7e18515aac1259809a096d5c6dba77ca5409
philippegeyaxa <philippe.gey@axa.fr>
2020-10-06 18:57:38 +0200

    performance improved to remain in the target with docker

--- src/main/java/tripmaster/gps/GpsServiceImpl.java
+++ src/main/java/tripmaster/gps/GpsServiceImpl.java
@@ -1,8 +1,11 @@
 package tripmaster.gps;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,10 +25,26 @@ import tripmaster.common.user.User;
  */
 @Service
 public class GpsServiceImpl implements GpsService {
+    private static final int NUMBER_OF_EXPECTED_USER_PARTITIONS = 10;
+    private static final int THREAD_POOL_SIZE = NUMBER_OF_EXPECTED_USER_PARTITIONS * 2;
 
 	private Logger logger = LoggerFactory.getLogger(GpsServiceImpl.class);
 	@Autowired private GpsUtil gpsUtil;
-
+	
+	// For performance reasons it is required to split users for submission on several threads
+	private List<List<User>> divideUserList(List<User> userList) {
+		List<List<User>> partitionList = new LinkedList<List<User>>();
+		int expectedSize = userList.size() / NUMBER_OF_EXPECTED_USER_PARTITIONS;
+		if (expectedSize == 0) {
+			partitionList.add(userList);
+			return partitionList;
+		}
+		for (int i = 0; i < userList.size(); i += expectedSize) {
+			partitionList.add(userList.subList(i, Math.min(i + expectedSize, userList.size())));
+		}
+		return partitionList;
+	}
+	
 	/**
 	 * Registers the current location for all users of the given list.
 	 * @param userList of all users for whom the current location shall be registered.
@@ -35,10 +54,23 @@ public class GpsServiceImpl implements GpsService {
 	@Override
 	public List<User> trackAllUserLocations(List<User> userList) {
 		logger.debug("trackAllUserLocations with list of size = " + userList.size());
-		userList.stream().parallel().forEach(user -> {
-			VisitedLocation visitedLocation = gpsUtil.getUserLocation(user.userId);
-			user.addToVisitedLocations(newVisitedLocationData(visitedLocation));
+		// The number of threads has been defined after several tests to match the performance target
+		ForkJoinPool forkJoinPool = new ForkJoinPool(THREAD_POOL_SIZE);
+		// Divide user list into several parts and submit work separately for these parts
+		divideUserList(userList).stream().parallel().forEach( partition -> {
+			try {
+				logger.debug("trackAllUserLocations submits calculation for user partition of size" +  partition.size());
+				forkJoinPool.submit( () -> partition.stream().parallel().forEach(user -> {
+					VisitedLocation visitedLocation = gpsUtil.getUserLocation(user.userId);
+					user.addToVisitedLocations(newVisitedLocationData(visitedLocation));
+				})).get();
+			} catch (InterruptedException | ExecutionException e) {
+				logger.error("trackAllUserLocations got an exception");
+				e.printStackTrace();
+				throw new RuntimeException("trackAllUserLocations got an exception");
+			}
 		});
+		forkJoinPool.shutdown();
 		return userList;
 	}
 
@@ -87,5 +119,5 @@ public class GpsServiceImpl implements GpsService {
 		return new VisitedLocationData(visitedLocation.userId,
 				new LocationData(visitedLocation.location.latitude, visitedLocation.location.longitude),
 				visitedLocation.timeVisited);
-	}
+	}	
 }
